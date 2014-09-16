7f2c62fca6bc81a935277fbd3301f0bdff2cf97f
Andriy Kyrylenko <andriy.kyrylenko@gmail.com>
2014-09-16 07:56:42 +0300

    init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ashop::Application.config.secret_token = '9e55bc82f392ecca9050a7fcce301f4810709f8ff04e05815e6418c174b33e9b5486dcabfcf0a50c55430ec7c453495ec004c1e90664d7a986db1f969d5b5c75'
